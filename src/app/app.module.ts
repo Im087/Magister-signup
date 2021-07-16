065b0f1e705999333eb4d19fe9742cf2ed7b334b
D.Lin <lindaqi087@gmail.com>
2021-07-16 04:49:14 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { RamaComponent } from './signup/rama/rama.component';
+import { ModalidadComponent } from './signup/modalidad/modalidad.component';
+import { TarifaComponent } from './signup/tarifa/tarifa.component';
+import { DatosComponent } from './signup/datos/datos.component';
+import { DireccionComponent } from './signup/direccion/direccion.component';
+import { PagoComponent } from './signup/pago/pago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    RamaComponent,
+    ModalidadComponent,
+    TarifaComponent,
+    DatosComponent,
+    DireccionComponent,
+    PagoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/signup/rama/rama.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rama',
+  templateUrl: './rama.component.html',
+  styleUrls: ['./rama.component.css']
+})
+export class RamaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
