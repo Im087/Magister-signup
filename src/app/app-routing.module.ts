e7e4184f91de6d12fd207f96bf70731e49a8ecf6
D.Lin <lindaqi087@gmail.com>
2021-07-16 05:20:44 +0200

    router configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,56 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './signup/signup.component';
+import { TarifaComponent } from './signup/tarifa/tarifa.component';
+import { DatosComponent } from './signup/datos/datos.component';
+import { DireccionComponent } from './signup/direccion/direccion.component';
+import { ModalidadComponent } from './signup/modalidad/modalidad.component';
+import { PagoComponent } from './signup/pago/pago.component';
+import { RamaComponent } from './signup/rama/rama.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'sign',
+    component: SignupComponent,
+    children: [
+      {
+        path: '',
+        component: RamaComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'rama',
+        component: RamaComponent
+      },
+      {
+        path: 'modalidad',
+        component: ModalidadComponent
+      },
+      {
+        path: 'tarifa',
+        component: TarifaComponent
+      },
+      {
+        path: 'datos',
+        component: DatosComponent
+      },
+      {
+        path: 'direccion',
+        component: DireccionComponent
+      },
+      {
+        path: 'pago',
+        component: PagoComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
